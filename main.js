3726a365c5856602a35008e0395eb9873a3ad4c7
naman1110 <86222625+naman1110@users.noreply.github.com>
2023-07-04 13:03:32 +0530

    Blockchain

---
+++ main.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+const routes = require('./routes');
+
+app.use(express.urlencoded());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
